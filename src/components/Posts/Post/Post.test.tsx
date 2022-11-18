55701e2796dc97288838b30eecf558ebea5f7f25
MrSetOne <lara.sanchez.michael@gmail.com>
2022-11-18 20:08:06 +0100

    jest works with framer motion transitions

--- src/components/Posts/Post/Post.test.tsx
+++ src/components/Posts/Post/Post.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Post from './Post'
 import { prettyDOM } from '@testing-library/react'
 import { renderWithProviders } from '../../../app/test-utils'
 import { post } from '../../../types'
+import { act } from 'react-dom/test-utils'
 
 interface stateExample {
   user: {
@@ -39,18 +40,24 @@ const stateExample: stateExample = {
 describe('<Post/>', () => {
   let component: any
 
-  beforeEach(() => {
-    component = renderWithProviders(<Post i={1} post={postExample} />, {
-      preloadedState: stateExample,
-    })
-  })
+  const doARender = (author: boolean = false) => {
+    component = null
+    component = renderWithProviders(
+      <Post i={1} post={{ ...postExample, userId: author ? 55 : 1 }} />,
+      {
+        preloadedState: stateExample,
+      },
+    )
+  }
 
   test('Renders post', () => {
+    doARender()
     const post = component.container.querySelector('.post')
     expect(post).toHaveClass('post')
   })
 
   test('Post contains correct title & body', () => {
+    doARender()
     const title = component.container.querySelector('.post .post_content h3')
     expect(title).toHaveTextContent('The title')
     const body = component.container.querySelector('.post .post_content p')
@@ -58,6 +65,7 @@ describe('<Post/>', () => {
   })
 
   test('Navigate in header', () => {
+    doARender()
     const link = component.container.querySelector('.post__header--link')
     if (link !== null) {
       fireEvent.click(link)
@@ -65,23 +73,35 @@ describe('<Post/>', () => {
     expect(mockedNavigate.mock.calls).toHaveLength(1)
   })
 
-  test(`Open & close menu works(Alien)`, () => {
+  test(`Open & close menu works(Alien)`, async () => {
+    doARender()
     const menu = component.container.querySelector('.PostMenu')
     const switchBtn = component.getByTestId('PostMenu--switch')
     expect(menu).toHaveStyle('width: 40px;')
     fireEvent.click(switchBtn)
-    setTimeout(() => {
-      expect(menu).toHaveStyle('width: 77px')
-    }, 300)
+    await waitFor(() => expect(menu).toHaveStyle('width: 77px'))
   })
 
   test('Edit button does not exist in alien post', () => {
+    doARender()
     const editBtn = screen.queryByTestId('PostMenu--edit')
     expect(editBtn).toBeNull()
   })
+
+  test('Open & close menu works(Author)', async () => {
+    doARender(true)
+    const menu = component.container.querySelector('.PostMenu')
+    const switchBtn = component.getByTestId('PostMenu--switch')
+    expect(menu).toHaveStyle('width: 40px;')
+    fireEvent.click(switchBtn)
+    await waitFor(() => expect(menu).toHaveStyle('width: 112px'))
+  })
+
+
+
+
   // TODO
   // ! MENU
-  // Open & close menu works(Author)
   // Edit button exist in your post
   // ! POST TOOLS
   // Mock delete post
